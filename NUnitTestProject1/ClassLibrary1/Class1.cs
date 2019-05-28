6068bcc79af7340f906dba44f31891afdc66a5aa
ashishbaldota <36469547+ashishbaldota@users.noreply.github.com>
2019-05-28 17:31:50 +0530

    Selenium C# UI Test
    Selenium C# UI Test

---
+++ NUnitTestProject1/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public string browser = "Chrome";
+        public bool IsPrime(int prime)
+        {
+            if (prime == 1)
+                return false;
+            throw new NotImplementedException("Please create a test first");
+
+        }
+
+
+    }
+}
---
+++ NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//using ClassLibrary1;
+//using System.Threading;
+//using System;
+
+
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        public string browser = "Chrome";
+       // private Class1 obj = new Class1();
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            // obj.IsPrime(1);
+            //IWebDriver webdriver;
+            if (browser.Equals("Chrome"))
+            {
+                IWebDriver driver = new ChromeDriver(@"C:\Users\ashish.baldota\source\repos\NUnitTestProject1\NUnitTestProject1\driver");
+                driver.Navigate().GoToUrl("http://shop.demoqa.com/");
+                
+                //List<IWebElement> allLinks=driver.FindElements(By.PartialLinkText("MyA"));
+               
+            }
+        }
+    }
+}
